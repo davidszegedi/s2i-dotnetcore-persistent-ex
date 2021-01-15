a52ade0eae0cc585c5497afb3f9b210b12242eb4
davidszegedi <49760874+davidszegedi@users.noreply.github.com>
2021-01-15 18:25:14 +0100

    Update Startup.cs

--- app/Startup.cs
+++ app/Startup.cs
@@ -66,8 +66,8 @@ namespace RazorPagesContacts
         {
             if (_migrateDatabase)
             {
-                Logger.LogInformation("Do not migrate database");
-                //MigrateDatabase(app);
+                Logger.LogInformation("Migrate database");
+                MigrateDatabase(app);
                 
             }
 
